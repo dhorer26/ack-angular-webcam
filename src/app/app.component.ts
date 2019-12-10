49712cfc2b07338618bcc564466c8b564a88d862
Ravindra Dhore <ravindrad@ansh.com>
2019-12-10 14:26:23 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+import { Http, Request } from '@angular/http';
+
+// imported here just for type checking. Optional
+import { WebCamComponent } from 'ack-angular-webcam';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firebase-auth-angular-app';
+  base64: string;
+
+  constructor(public http: Http) {}
+
+  genBase64( webcam: WebCamComponent ) {
+    webcam.getBase64()
+    .then( base => this.base64 = base)
+    .catch( e => console.error(e) );
+  }
+
+  // get HTML5 FormData object and pretend to post to server
+  genPostData( webcam: WebCamComponent ) {
+    webcam.captureAsFormData({fileName: 'file.jpg'})
+    .then( formData => this.postFormData(formData) )
+    .catch( e => console.error(e) );
+  }
+
+  // a pretend process that would post the webcam photo taken
+  postFormData(formData: FormData) {
+    const config = {
+      method: 'post',
+      url: 'http://www.aviorsciences.com/',
+      body: formData
+    };
+
+    const request = new Request(config);
+
+    return this.http.request( request );
+  }
+
+  onCamError(err: any) {}
+
+  onCamSuccess() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { WebCamModule } from 'ack-angular-webcam';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    WebCamModule,
+    // tslint:disable-next-line: deprecation
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
